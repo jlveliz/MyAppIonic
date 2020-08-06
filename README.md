ab3bfaa465b0bbeed345bdb6c0195f473540022d
Jorge Luis Veliz <jorgeconsalvacion@gmail.com>
2020-08-06 12:52:28 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,13 @@
+My First Ionic App
+==========
+**My first App** example 
+
+Jorge Luis Veliz
+
+* Angular 7
+* @ionic/angular 4.0.0
+* Ionic Native 5
+* 2 Page modals
+* 2 Components
+
+https://thejlmedia.com
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController, MenuController, ToastController, AlertController, LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  public onLoginForm: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public menuCtrl: MenuController,
+    public toastCtrl: ToastController,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
+
+  ngOnInit() {
+
+    this.onLoginForm = this.formBuilder.group({
+      'email': [null, Validators.compose([
+        Validators.required
+      ])],
+      'password': [null, Validators.compose([
+        Validators.required
+      ])]
+    });
+  }
+
+  async forgotPass() {
+    const alert = await this.alertCtrl.create({
+      header: 'Olvidó contraseña?',
+      message: 'Ingrese un correo valido.',
+      inputs: [
+        {
+          name: 'email',
+          type: 'email',
+          placeholder: 'Email'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirmar Cancelca');
+          }
+        }, {
+          text: 'Confirmar',
+          handler: async () => {
+            const loader = await this.loadingCtrl.create({
+              duration: 2000
+            });
+
+            loader.present();
+            loader.onWillDismiss().then(async l => {
+              const toast = await this.toastCtrl.create({
+                showCloseButton: true,
+                message: 'El correo fue enviado correctamente.',
+                duration: 3000,
+                position: 'bottom'
+              });
+
+              toast.present();
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  // // //
+  goToRegister() {
+    this.navCtrl.navigateRoot('/register');
+  }
+
+  goToHome() {
+    this.navCtrl.navigateRoot('/home-results');
+  }
+
+}
